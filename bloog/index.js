7f4d1582467ad11a440b189ffe6303b72780bd2f
aryan <aryanarmaan1912@gmail.com>
2024-12-05 22:45:54 +0000

    dependencies and break code

--- bloog/index.js
+++ bloog/index.js
@@ -4,18 +4,37 @@ import express from "express";
 //all the values
 const app = express();
 const port = 3000
-var data ={
+
+var posts = [Post("hello","hello world")]
+var data = {
     posts : posts
 }
-var post = {}
 //requests
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port,()=>{
     console.log('listening on port : ' + port)
 })
 
+app.get("/create",(req,res)=>{
+    res.sendFile("/html/index.html")
+    console.log("page delivered")
+})
 app.get("/",(req,res)=>{
     res.render("index.ejs",data)
 })
+app.put("/submit",(req,res)=>{
+    res.sendFile("html/create.html")
+    console.log(req.body)
+})
 //functions(if needed)
+function Post(title,text, image){
+    this.title = title;
+    this.text = text;
+    this.image = image;
+}
+
+
+//post1
+//push("Post"+(posts.length+1))
